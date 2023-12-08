a3dc4abaad33ef29ab3a1d39a9397c4b63953d76
Luca Mattiazzi <luca.mattiazzi@illimity.com>
2023-12-08 16:49:36 +0100

    d5a

---
+++ src/bin/day5-1.rs
@@ -0,0 +1,59 @@
+use std::env;
+use std::fs;
+
+fn convert_in_range(ranges: Vec<Vec<i64>>, number: i64) -> i64 {
+    for range in ranges {
+        let from_start = range[1];
+        let from_end = range[1] + range[2];
+        let to_start = range[0];
+        if number >= from_start && number < from_end {
+            return to_start + number - range[1];
+        }
+    }
+    return number;
+}
+
+fn converter(all_ranges: Vec<Vec<Vec<i64>>>, starting_number: i64) -> i64 {
+    let mut number = starting_number;
+    for ranges in all_ranges {
+        number = convert_in_range(ranges, number);
+    }
+    return number;
+}
+
+fn main() {
+    let file_path: String = env::args().nth(1).expect("Please supply a file path");
+    let file_content: String = fs::read_to_string(file_path).unwrap();
+    let mut seeds: Vec<i64> = Vec::new();
+    let mut all_ranges: Vec<Vec<Vec<i64>>> = Vec::new();
+    for block in file_content.split("\n\n") {
+        let lines = block.lines().collect::<Vec<&str>>();
+        if lines.len() == 1 {
+            lines[0]
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" ")
+                .for_each(|seed| {
+                    println!("{:?}", seed);
+                    seeds.push(seed.parse::<i64>().unwrap());
+                });
+        } else {
+            let mut ranges: Vec<Vec<i64>> = Vec::new();
+            for (index, line) in lines.iter().enumerate() {
+                if index > 0 {
+                    let range: Vec<i64> =
+                        line.split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
+                    ranges.push(range);
+                }
+            }
+            all_ranges.push(ranges);
+        }
+    }
+    let converted_numbers = seeds
+        .iter()
+        .map(|x| converter(all_ranges.clone(), *x))
+        .collect::<Vec<i64>>();
+    let min = *converted_numbers.iter().min().unwrap();
+    println!("min: {:?}", min);
+}
